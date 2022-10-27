306a849713a29e4e35ae2ed3fcb6eb6f4a9b5dd1
Haridev003 <112387476+Haridev003@users.noreply.github.com>
2022-10-27 15:16:38 +0530

    Create Parse.java

---
+++ JAVA_TEST_PRACTICE/Parse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+import java.lang.*;
+class Main{
+    public static void main(String[] args) {
+        String str="9999";
+        long varLong = Long.parseLong(str);
+        System.out.println(varLong);
+    }
+    }
